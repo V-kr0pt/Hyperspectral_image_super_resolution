c5fed10b2ff905c4b3cfcaae1fe597a005ff17ca
Oussama ZAIBI <oussama.zaibi@supcom.tn>
2023-06-18 04:52:48 +0200

    cleaned the code

--- preprocessing.py
+++ preprocessing.py
@@ -9,54 +9,21 @@ class Dataset(data.Dataset):
     def __init__(self, img, isTrain=True):
         super(Dataset, self).__init__()
 
-        #self.args = args
-        #self.sp_matrix = sp_matrix
-        #self.msi_channels = sp_matrix.shape[1]
-        
         self.img = img
         self.isTrain = isTrain
 
-        default_datapath = os.getcwd()
-        """data_folder = os.path.join(default_datapath, args.data_name)
-        if os.path.exists(data_folder):
-            for root, dirs, files in os.walk(data_folder):
-                if args.mat_name in files:
-                    raise Exception("HSI data path does not exist!")
-                else:
-                    data_path = os.path.join(data_folder, args.mat_name+'.mat')
-        else:
-            return 0
-        
-        #self.imgpath_list = sorted(glob.glob(data_path))
-        #self.img_list = []
-        for i in range(len(self.imgpath_list)):
-            self.img_list.append(io.loadmat(self.imgpath_list[i])['img'])
-
-        'for single HSI'
-        (_, _, self.hsi_channels) = img.shape
-
-        'generate simulated data'
-        self.img_patch_list = []
-        self.img_lr_list = []
-        self.img_msi_list = []
-        for i, img in enumerate(self.img_list):
-        """ 
         (h, w, c) = self.img.shape
         s = 4 #scale_factor See table 1 page 6 in the article
         self.sigma = 0.5 #See Section B in the article
         'Ensure that the side length can be divisible'
         r_h, r_w = h%s, w%s
-        #img_patch = self.img[int(r_h/2):h-(r_h-int(r_h/2)),int(r_w/2):w-(r_w-int(r_w/2)),:]
-        #self.img_patch_list.append(img_patch)
 
         'LrHSI'
         self.img_lr = self.generate_LrHSI(s)
-        #self.img_lr_list.append(img_lr)
         (self.lrhsi_height, self.lrhsi_width, _) = self.img_lr.shape
 
         'HrMSI'
         self.img_msi = self.generate_HrMSI()
-        #self.img_msi_list.append(img_msi)
 
     def downsamplePSF(self, stride=1, sigma=0.5):
         def matlab_style_gauss2D(shape=(3,3), sigma=0.5):
@@ -87,17 +54,6 @@ class Dataset(data.Dataset):
         img_lr = self.downsamplePSF(sigma=self.sigma, stride=scale_factor)
         return img_lr
 
-    
-    """
-    def generate_HrMSI(self, img, sp_matrix):
-        (h, w, c) = img.shape
-        self.msi_channels = sp_matrix.shape[1]
-        if sp_matrix.shape[0] == c:
-            img_msi = np.dot(img.reshape(w*h,c), sp_matrix).reshape(h,w,sp_matrix.shape[1])
-        else:
-            raise Exception("The shape of sp matrix does not match the image")
-        return img_msi
-    """
 
     def generate_HrMSI(self):
         srf_bands = {
@@ -133,18 +89,5 @@ class Dataset(data.Dataset):
             msi_data[..., i] = np.mean(self.img[..., start_band:end_band+1], axis=-1)
         return msi_data
 
-    """def __getitem__(self):
-        img_patch = self.img_patch_list[index]
-        img_lr = self.img_lr_list[index]
-        img_msi = self.img_msi_list[index]
-        img_name = os.path.basename(self.imgpath_list[index]).split('.')[0]
-        img_tensor_lr = torch.from_numpy(img_lr.transpose(2,0,1).copy()).float()
-        img_tensor_hr = torch.from_numpy(img_patch.transpose(2,0,1).copy()).float()
-        img_tensor_rgb = torch.from_numpy(img_msi.transpose(2,0,1).copy()).float()
-        return {"lhsi":img_tensor_lr,
-                'hmsi':img_tensor_rgb,
-                "hhsi":img_tensor_hr,
-                "name":img_name}
-    """
     def __len__(self):
         return len(self.imgpath_list)
